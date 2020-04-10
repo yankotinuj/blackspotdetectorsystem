a6f79cad74ee33bc4f6dae6110bc80c5adbe0381
yankotinuj <yankotinu.23@gmail.com>
2020-04-10 18:26:28 +0700

    Adding route to User Profile Page

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'HomeController@index')->name('home');
+
+Route::get('/dashboard/user', 'UserController@index')->name('userprofile');
