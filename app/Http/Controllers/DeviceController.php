655c374e39094eba66c911f9922206f186dde7a8
yankotinuj <yankotinu.23@gmail.com>
2020-04-11 13:07:51 +0700

    Adding Device Controller

---
+++ app/Http/Controllers/DeviceController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Device;
+use Illuminate\Http\Request;
+use Auth;
+
+class DeviceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $devices = Device::where('deviceid', Auth::user()->deviceid)->first();
+        return view('device.device', ['devices' => $devices]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Device  $device
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Device $device)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Device  $device
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Device $device)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Device  $device
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Device $device)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Device  $device
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Device $device)
+    {
+        //
+    }
+}
