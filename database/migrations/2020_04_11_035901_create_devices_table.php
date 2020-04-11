8aa191cd9263a7642f8395f98d77a7622963e956
yankotinuj <yankotinu.23@gmail.com>
2020-04-11 13:08:13 +0700

    Adding Device Migration

---
+++ database/migrations/2020_04_11_035901_create_devices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('deviceid')->unique();
+            $table->string('devicename');
+            $table->string('deviceversion');
+            $table->string('devicecode');
+            $table->string('devicemicrocontroller');
+            $table->string('deviceurl');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+}
