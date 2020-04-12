443e7b101c89e086826e64f39d0979295db4fad0
yankotinuj <yankotinu.23@gmail.com>
2020-04-12 18:06:37 +0700

    Adding Location Model

---
+++ app/Location.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends Model
+{
+    protected $table = 'locations';
+    protected $fillable = [
+        'id','locationid','lat','lng','alamat'
+    ];
+}
