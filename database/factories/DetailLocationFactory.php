6f0de846059b69d10022cb3bfcf58a0d79b6cfea
yankotinuj <yankotinu.23@gmail.com>
2020-04-15 19:22:54 +0700

    Adding DetailLocation Factory

---
+++ database/factories/DetailLocationFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\DetailLocation;
+use Faker\Generator as Faker;
+
+$factory->define(DetailLocation::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
