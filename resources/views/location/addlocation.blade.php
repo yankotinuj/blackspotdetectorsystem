97f8f4349642925366007872ce36589c1380eb56
yankotinuj <yankotinu.23@gmail.com>
2020-04-26 23:41:08 +0700

    Adding JavaScript for store new location use Axios

--- resources/views/location/addlocation.blade.php
+++ resources/views/location/addlocation.blade.php
@@ -8,15 +8,16 @@
             <h5>{{$lastlocationid}}</h5>
             <h5>{{$extractedNumLocationId}}</h5>
             <h5>{{$newNumLocationId}}</h5>
+            <h5>{{$stringNewNumLocationId}}</5>
         </div>
         <div class="card-body bg-light text-body">  
-            <form id="tambahlokasi" method="POST" action="javascript:void(0)">
+            <form id="tambahlokasi" method="post" action="javascript:void(0)">
 
                 <div class="form-group">
                     <div class="row">
                         <label for="latitude" class="col-4 col-form-label">Latitude Lokasi</label>
                             <div class="col-8">
-                                <input class="form-control" type="number" placeholder="Latitude Lokasi" id="latitude" required>
+                                <input class="form-control" type="number" placeholder="Latitude Lokasi" id="lat" required>
                             </div>
                     </div>
                     <div class="row pt-2">
@@ -33,7 +34,7 @@
                     <div class="row">
                         <label for="longitude" class="col-4 col-form-label">Longitude Lokasi</label>
                             <div class="col-8">
-                                <input class="form-control" type="number" placeholder="Longitude Lokasi" id="longitude" required>
+                                <input class="form-control" type="number" placeholder="Longitude Lokasi" id="lng" required>
                             </div>
                     </div>
                     <div class="row pt-2">
@@ -60,9 +61,41 @@
                     <label class="form-check-label" for="confirmationCheck">Dengan mencetang checkbox ini, Saya bertanggung jawab dengan data yang di submit.</label>
                 </div>
                 
-                <button type="submit" onclick="#" id="submitForm" class="btn btn-success">Submit</button> &nbsp;
+                <button type="submit" onclick="onSubmitClicked();" id="submitForm" class="btn btn-success">Simpan</button> &nbsp;
                 <a class="btn btn-info text-white" href="{{ route('location-by-list') }}" role="button">Kembali</a> &nbsp;
                 <button type="reset" class="btn btn-danger">Reset</button>
+
+                <script>
+                    const options = {
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        }
+                    };
+                    async function onSubmitClicked()
+                    {
+                        $("#submitForm").html('Menyimpan...');
+                        axios.post('{{ route("save-location") }}', {
+                            locationid: 'DRK{{$stringNewNumLocationId}}',
+                            lat: jQuery('#lat').val(),
+                            lng: jQuery('#lng').val(),
+                            alamat: jQuery('#alamat').val(),
+                            verified: 0,
+                            addedby: '{{$users->username}}'
+                        }, options)
+                        .then(function (response)
+                        {
+                            Command: swal("Sukses Menyimpan", "Lokasi Berhasil Ditambahkan", "success");
+                            console.log(response);
+                            $("#submitForm").html('Simpan');
+                        })
+                        .catch(function (error)
+                        {
+                            Command: swal("Gagal Menyimpan", "Lokasi Gagal Ditambahkan atau Form Belum Lengkap Diisi", "error");
+                            $("#submitForm").html('Simpan');
+                            console.log(error);
+                        });
+                    }
+                </script>
 
             </form>
         </div>
