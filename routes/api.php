efc2f2e25e8265bbfbf49a8cb6aeb7de7f621f1f
yankotinuj <yankotinu.23@gmail.com>
2020-04-26 23:41:28 +0700

    Adding route for store (post) use Axios

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/dashboard/location/add-location/save', 'LocationController@store')->name('save-location');
