707546767a26a90ec5ad2a9211a19e436eabadf5
yankotinuj <yankotinu.23@gmail.com>
2020-05-05 17:01:07 +0700

    Create Location by Maps Page

---
+++ resources/views/location/locationbymaps.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.map')
+@section('content')
+<div class="container">
+    <h4>Lokasi Daerah Rawan Kecelakaan</h4>
+
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('location-by-list') }}">Daftar</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Peta</a>
+        </li>
+    </ul>
+
+    <div class="card my-3">
+        <div class="card-body">
+            {!! $map['html'] !!}
+        </div>
+    </div>
+    
+</div>
+@endsection
