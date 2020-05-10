3d5b4f0c69259b6a84d079a8bc6d27a0e1476f74
yankotinuj <yankotinu.23@gmail.com>
2020-05-10 23:16:02 +0700

    Adding Laravel Charts Library

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         FarhanWazir\GoogleMaps\GMapsServiceProvider::class,
-
+        ConsoleTVs\Charts\ChartsServiceProvider::class,
     ],
 
     /*
@@ -227,7 +227,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'GMaps' => FarhanWazir\GoogleMaps\Facades\GMapsFacade::class,
-
     ],
 
 ];
