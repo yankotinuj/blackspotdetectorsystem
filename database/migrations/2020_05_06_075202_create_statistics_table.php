525d44f20a2550228d6570238d11cddd92291fb4
yankotinuj <yankotinu.23@gmail.com>
2020-05-10 23:19:10 +0700

    Create Statistics Migration

---
+++ database/migrations/2020_05_06_075202_create_statistics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatisticsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statistics', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('deviceid');
+            $table->string('locationid');
+            $table->integer('spd_1500m');
+            $table->integer('spd_1000m');
+            $table->integer('spd_500m');
+            $table->integer('spd_10m');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statistics');
+    }
+}
