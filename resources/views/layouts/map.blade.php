c755ccd7fb44d6fee455725bc6f2d504d60acc58
yankotinuj <yankotinu.23@gmail.com>
2020-05-19 15:32:45 +0700

    Fixing UI

--- resources/views/layouts/map.blade.php
+++ resources/views/layouts/map.blade.php
@@ -28,7 +28,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light shadow-sm">
             <div class="container">
-                <a class="navbar-brand text-white" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -46,33 +46,33 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link text-white" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
                             <li class="nav-item px-1">
-                                <a class="nav-link text-white btn btn-info" href="{{ route('location-by-list') }}">Lokasi Daerah Rawan Kecelakaan</a>
+                                <a class="nav-link btn btn-info" href="{{ route('location-by-list') }}">Lokasi Daerah Rawan Kecelakaan</a>
                             </li>
                             <li class="nav-item px-1">
-                                <a class="nav-link text-white btn btn-info" href="{{ route('device') }}">Perangkat Saya</a>
+                                <a class="nav-link btn btn-info" href="{{ route('device') }}">Perangkat Saya</a>
                             </li>
                             <li class="nav-item px-1">
-                                <a class="nav-link text-white btn btn-info" href="{{ route('statistics') }}">Statisktik</a>
+                                <a class="nav-link btn btn-info" href="{{ route('statistics') }}">Statisktik</a>
                             </li>
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item text-white" href="{{ route('userprofile') }}">
+                                    <a class="dropdown-item" href="{{ route('userprofile') }}">
                                         View Profile
                                     </a>
-                                    <a class="dropdown-item text-white" href="{{ route('logout') }}"
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
