b88c86f38439f7db3977607361ee2fbdb50ef988
yankotinuj <yankotinu.23@gmail.com>
2020-07-09 01:10:50 +0700

    Tidy Up Routes and Create Some Routes

--- routes/web.php
+++ routes/web.php
@@ -23,41 +23,11 @@ Auth::routes();
 
 Route::get('/dashboard', 'HomeController@index')->name('home');
 
-Route::get('/dashboard/user', 'UserController@index')->name('userprofile');
+Route::get('/dashboard/profile', 'UserController@index')->name('userprofile');
 
 Route::get('/dashboard/device', 'DeviceController@index')->name('device');
 
 Route::get('/dashboard/statistics', 'StatisticController@index')->name('statistics');
-
-/* ========== Bagian Menu Permintaan Tambah Lokasi ========== */
-
-Route::get('/dashboard/location/added-by-user', 'LocationController@addedlocation')->name('location-added-by-user');
-
-Route::get('/dashboard/location/added-by-user/{locationid}', 'DetailLocationController@index')->name('location-added-by-user-detail');
-
-Route::get('/dashboard/location/added-by-user/{locationid}/add-detail', 'DetailLocationController@adddetail')->name('location-added-by-user-detail-add');
-
-Route::get('/dashboard/location/added-by-user/{locationid}/delete', 'LocationController@delete')->name('location-added-by-user-delete');
-
-Route::get('/dashboard/location/added-by-user/{locationid}/verify', 'LocationController@verifyLocation')->name('location-added-by-user-verify');
-
-//Route::post('/dashboard/location/added-by-user/{locationid}/add-detail/update', 'DetailLocationController@update')->name('location-added-by-user-detail-add-update');
-
-/* ========== ==================================== ========== */
-
-/* =============== Bagian Menu Kelola Lokasi ================ */
-
-Route::get('/dashboard/location/manage', 'LocationController@indexManageLocation')->name('location-manage');
-
-Route::get('/dashboard/location/manage/{locationid}/edit', 'LocationController@edit')->name('location-manage-edit');
-
-Route::get('/dashboard/location/manage/{locationid}/detail', 'DetailLocationController@index')->name('location-manage-detail');
-
-Route::get('/dashboard/location/manage/{locationid}/detail/edit', 'DetailLocationController@adddetail')->name('location-manage-detail-edit');
-
-Route::get('/dashboard/location/manage/{locationid}/delete', 'LocationController@delete')->name('location-manage-delete');
-
-/* ========== ==================================== ========== */
 
 Route::get('/dashboard/location/by-maps', 'MapsController@index')->name('location-by-maps');
 
@@ -66,3 +36,55 @@ Route::get('/dashboard/location/by-list', 'LocationController@index')->name('loc
 Route::get('/dashboard/location/by-list/{locationid}', 'DetailLocationController@index')->name('location-by-list-detail');
 
 Route::get('/dashboard/location/add-location', 'LocationController@addLocation')->name('add-location');
+
+/* ========== Routes Untuk Admin ========== */
+
+Route::get('/dashboard/admin', 'HomeController@indexAdmin')->name('home-admin');
+
+Route::get('/dashboard/admin/profile', 'UserController@indexAdmin')->name('userprofile-admin');
+
+Route::get('/dashboard/admin/user/{username}', 'UserController@viewUser')->name('viewuserprofile');
+
+Route::get('/dashboard/admin/device/{deviceid}', 'DeviceController@viewDevice')->name('viewdevice');
+
+//Bagian Menu Lokasi Daerah Rawan Kecelakaan
+
+Route::get('/dashboard/admin/location/by-maps', 'MapsController@index')->name('location-by-maps-admin');
+
+Route::get('/dashboard/admin/location/by-list', 'LocationController@index')->name('location-by-list-admin');
+
+Route::get('/dashboard/admin/location/by-list/{locationid}', 'DetailLocationController@index')->name('location-by-list-detail-admin');
+
+Route::get('/dashboard/admin/location/add-location', 'LocationController@addLocation')->name('add-location-admin');
+
+//Bagian Menu Kelola Lokasi
+
+Route::get('/dashboard/admin/location/manage', 'LocationController@indexManageLocation')->name('location-manage');
+
+Route::get('/dashboard/admin/location/manage/{locationid}/edit', 'LocationController@edit')->name('location-manage-edit');
+
+Route::get('/dashboard/admin/location/manage/{locationid}/detail', 'DetailLocationController@index')->name('location-manage-detail');
+
+Route::get('/dashboard/admin/location/manage/{locationid}/detail/edit', 'DetailLocationController@adddetail')->name('location-manage-detail-edit');
+
+Route::get('/dashboard/admin/location/manage/{locationid}/delete', 'LocationController@delete')->name('location-manage-delete');
+
+//Bagian Menu Permintaan Tambah Lokasi
+
+Route::get('/dashboard/admin/location/added-by-user', 'LocationController@addedlocation')->name('location-added-by-user');
+
+Route::get('/dashboard/admin/location/added-by-user/{locationid}', 'DetailLocationController@index')->name('location-added-by-user-detail');
+
+Route::get('/dashboard/admin/location/added-by-user/{locationid}/add-detail', 'DetailLocationController@adddetail')->name('location-added-by-user-detail-add');
+
+Route::get('/dashboard/admin/location/added-by-user/{locationid}/delete', 'LocationController@delete')->name('location-added-by-user-delete');
+
+Route::get('/dashboard/admin/location/added-by-user/{locationid}/verify', 'LocationController@verifyLocation')->name('location-added-by-user-verify');
+
+//Route::post('/dashboard/location/added-by-user/{locationid}/add-detail/update', 'DetailLocationController@update')->name('location-added-by-user-detail-add-update');
+
+//Bagian Menu Statistik
+
+Route::get('/dashboard/admin/statistics', 'StatisticController@indexAdmin')->name('statistics-admin');
+
+/* ======================================== */
