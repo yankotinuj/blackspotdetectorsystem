0a18b9d9681c2b391308c517993153a4562aac32
yankotinuj <yankotinu.23@gmail.com>
2020-07-17 00:09:05 +0700

    Finishing UI and Add SweetAlert

--- resources/views/location/locationadded.blade.php
+++ resources/views/location/locationadded.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-    <div class="card text-white bg-primary">
-        <div class="card-header pt-4">
+    <div class="card">
+        <div class="card-header bg-white pt-4">
             <h4>Permintaan Tambah Lokasi</h4>
         </div>
-        <div class="card-body bg-light text-body">  
+        <div class="card-body">  
             @if ($totallocationadded == 0)
                 <div class="alert alert-primary" role="alert">
                     Tidak ada permintaan tambah lokasi.
                 </div>
-                <a class="btn btn-danger" href="{{ url()->previous() }}" role="button">Kembali</a>
+                <a class="btn btn-danger" href="{{ route('location-by-list-admin') }}" role="button">Kembali</a>
 
             @else
+            <div class="table-responsive">
                 <table class="table table-striped table-hover">
                     <thead>
                     <tr>
@@ -31,11 +32,74 @@
                         <td id="locAddress">{{$lokasi->alamat}}</td>
                         <td id="locAddedBy">{{$lokasi->addedby}}</td>
                         <td>
-                            <a class="btn btn-success" role="button" href="{{ route('location-added-by-user-detail',$lokasi->locationid) }}">Detail</a> &nbsp;
-                            <a class="btn btn-light" role="button" href="https://www.google.com/maps/place/{{$lokasi->lat}},{{$lokasi->lng}}" target="_blank">Lihat</a> &nbsp;
-                            <a class="btn btn-info" role="button" href="{{ route('location-added-by-user-verify',$lokasi->locationid) }}">Verifikasi</a> &nbsp;
-                            <a class="btn btn-danger" role="button" href="{{ route('location-added-by-user-delete',$lokasi->locationid) }}">Delete</a> &nbsp;
-                            <!--<button class="btn btn-danger" onclick="deleteLocation()">Delete</button> &nbsp;-->
+                            <a class="btn btn-primary" role="button" href="{{ route('location-added-by-user-detail',$lokasi->locationid) }}">Detail</a> &nbsp;
+                            <a class="btn btn-info text-white" role="button" href="https://www.google.com/maps/place/{{$lokasi->lat}},{{$lokasi->lng}}" target="_blank">Lihat</a> &nbsp;
+                            <a id="verifyRequest{{$lokasi->locationid}}" class="btn btn-success" role="button" href="{{ route('location-added-by-user-verify',$lokasi->locationid) }}">Verifikasi</a> &nbsp;
+                            <a id="deleteRequest{{$lokasi->locationid}}" class="btn btn-danger" role="button" onclick="deleteLocation();" href="{{ route('location-added-by-user-delete',$lokasi->locationid) }}">Delete</a>
+                        
+                            <script>
+                                $(document).ready(function(){
+                                    $("#deleteRequest{{$lokasi->locationid}}").on('click', function(e){
+                                        e.preventDefault();
+
+                                        var href = $(this).attr('href');
+
+                                        swal(
+                                        {
+                                            title: "Anda Yakin?",
+                                            text: "Lokasi ini akan dihapus secara permanen!",
+                                            icon: "warning",
+                                            dangerMode: true,
+                                            buttons: {
+                                                cancel: "Batal",
+                                                confirm: "Hapus Lokasi",
+                                            },
+                                            closeOnClickOutside: false,
+                                            closeOnEsc: false,
+                                        })
+                                        .then((willDelete) => {
+                                            if (willDelete)
+                                            {
+                                                swal("Lokasi Dihapus!", "Lokasi ini tidak terdapat lagi di database.", "success");
+                                                window.location.href = href;
+                                            }
+                                            else
+                                            {
+                                                swal("Aksi Dibatalkan!", "Lokasi ini batal dihapuskan.", "info");
+                                            }
+                                        });
+                                    });
+                                    $("#verifyRequest{{$lokasi->locationid}}").on('click', function(e){
+                                        e.preventDefault();
+
+                                        var href = $(this).attr('href');
+
+                                        swal(
+                                        {
+                                            title: "Anda Yakin?",
+                                            text: "Pastikan Lokasi akan diverifikasi!",
+                                            icon: "warning",
+                                            buttons: {
+                                                cancel: "Batal",
+                                                confirm: "Verifikasi Lokasi",
+                                            },
+                                            closeOnClickOutside: false,
+                                            closeOnEsc: false,
+                                        })
+                                        .then((willDelete) => {
+                                            if (willDelete)
+                                            {
+                                                swal("Lokasi Diverifikasi!", "Lokasi ini sudah terverifikasi dan ditampilkan di daftar lokasi daerah rawan kecelakaan.", "success");
+                                                window.location.href = href;
+                                            }
+                                            else
+                                            {
+                                                swal("Aksi Dibatalkan!", "Lokasi ini batal diverifikasi.", "info");
+                                            }
+                                        });
+                                    });
+                                });
+                            </script>
                         </td>
                     </tr>
                     @endforeach
@@ -44,32 +108,7 @@
                     @endif
                     </tbody>
                 </table>
-
-                <script>
-                    /*function deleteLocation()
-                    {
-                        var x = document.getElementById("myRadio").value;
-                        document.getElementById("demo").innerHTML = x;
-                    }*/
-                    /*function deleteLocation()
-                    {
-                        swal(
-                        {
-                            title: "Anda Yakin?",
-                            text: "Lokasi ini akan dihapus secara permanen!",
-                            type: "warning",
-                            showCancelButton: true,
-                            confirmButtonClass: "btn-danger",
-                            confirmButtonText: "Hapus",
-                            closeOnConfirm: false
-                        },
-                        function()
-                        {
-                            swal("Lokasi Dihapus!", "Lokasi ini tidak terdapat lagi di database.", "success");
-                        },
-                        );
-                    }*/
-                </script>
+            </div>
             @endif
 
         </div>
