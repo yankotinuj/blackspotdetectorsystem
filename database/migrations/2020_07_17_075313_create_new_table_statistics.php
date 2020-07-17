2413ca61fdec7a90a7f744d120c33216cfeff03d
yankotinuj <yankotinu.23@gmail.com>
2020-07-17 14:54:14 +0700

    Create New Statistics Migration

---
+++ database/migrations/2020_07_17_075313_create_new_table_statistics.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewTableStatistics extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statistics', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('deviceid');
+            $table->string('locationid');
+            $table->double('spd_1500m')->nullable();
+            $table->double('spd_1000m')->nullable();
+            $table->double('spd_500m')->nullable();
+            $table->double('spd_10m')->nullable();
+            $table->double('dst_1500m')->nullable();
+            $table->double('dst_1000m')->nullable();
+            $table->double('dst_500m')->nullable();
+            $table->double('dst_10m')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statistics');
+    }
+}
